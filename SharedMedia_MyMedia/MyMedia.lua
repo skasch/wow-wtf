67d426c7fdc9877235d589867eb75051db7beabb
Romain Mondon-Cancel <romain.mondon-cancel@ubisoft.com>
2018-10-12 15:03:22 -0700

    Adding SharedMedia_MyMedia

---
+++ SharedMedia_MyMedia/MyMedia.lua
@@ -0,0 +1,50 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+local MediaType_SOUND = LSM.MediaType.SOUND
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Aggro!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Interrupt!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Outheal!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Come close!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Kite away!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Purge!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Control!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Kite mob!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Soak!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Defense!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Look away!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Spread out!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Dispel!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Move around!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Stack up!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Dodge!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Move away!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Step aside!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Focus mob!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Move far!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Stop cast!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Focus!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Move mobs!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Stop DPS!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Hide for LoS!.ogg]])
+LSM:Register(MediaType_SOUND, [[Interface\Addons\SharedMedia_MyMedia\sound\AA_Move out!.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
